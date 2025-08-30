49a464188cf5476ad1e31c0628e0550c4a409ecc
Jose Tejada <jtejadavilca@gmail.com>
2025-08-30 11:54:37 +0100

    Using useOptimistic and useTransition (new in react 19), and adding sonner for toasting messages

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "sonner";
 //import { HooksApp } from "./HooksApp";
 //import { TrafficLight } from "./01-useState/TrafficLight";
 //import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect";
@@ -9,12 +10,14 @@ import { createRoot } from "react-dom/client";
 // import { TasksApp } from "./05-useReducer/TaskApp";
 // import { ScrambleWords } from "./05-useReducer/ScrambleWords";
 // import { MemoHook } from "./06-memos/MemoHook";
-import { MemoCounter } from "./06-memos/MemoCounter";
+// import { MemoCounter } from "./06-memos/MemoCounter";
+import { InstagromApp } from "./07-useOptimistic/InstagromApp";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
+        <Toaster />
         {/* <HooksApp /> */}
         {/* <TrafficLight /> */}
         {/* <TrafficLightWithEffect /> */}
@@ -23,6 +26,7 @@ createRoot(document.getElementById("root")!).render(
         {/* <FocusScreen /> */}
         {/* <TasksApp /> */}
         {/* <ScrambleWords /> */}
-        <MemoCounter />
+        {/* <MemoCounter /> */}
+        <InstagromApp />
     </StrictMode>
 );
